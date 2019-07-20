66d5f17348424a39f81cb646f0e8c1bc54791b44
Daniel <heyboy6059@gmail.com>
2019-07-20 14:50:43 +1000

    Created main screen, graphic, image (1980 x 1020) and added background file

--- src/beatmuse/BeatMuse.java
+++ src/beatmuse/BeatMuse.java
@@ -1,9 +1,17 @@
 package beatmuse;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class BeatMuse extends JFrame {
 
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
 	
 	public BeatMuse() {
 		setTitle("BeatMuse");
@@ -12,5 +20,19 @@ public class BeatMuse extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/MainBackground.jpg")).getImage();
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
 	}
 }
--- src/beatmuse/Main.java
+++ src/beatmuse/Main.java
@@ -3,8 +3,8 @@ package beatmuse;
 public class Main {
 
 	
-	public static final int SCREEN_WIDTH = 1280;
-	public static final int SCREEN_HEIGHT = 720;
+	public static final int SCREEN_WIDTH = 1920;
+	public static final int SCREEN_HEIGHT = 1080;
 	
 	public static void main(String[] args) {
 		
