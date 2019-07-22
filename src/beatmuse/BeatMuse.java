f20752e23b32f76c0484a25cae6d6a8a2cfbbb03
Daniel <heyboy6059@gmail.com>
2019-07-22 11:18:25 +1000

    Added MouseListener, MouseMotionListener on menu bar to make the whole screen is movable by mouse click and drag

--- src/beatmuse/BeatMuse.java
+++ src/beatmuse/BeatMuse.java
@@ -3,8 +3,12 @@ package beatmuse;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -16,6 +20,10 @@ public class BeatMuse extends JFrame {
 	private Image introBackground = new ImageIcon(Main.class.getResource("../images/MainBackground_title.jpg")).getImage();
 	
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/topbar.png")));
+	
+	private JButton closeButton = new JButton(new ImageIcon(Main.class.getResource("../images/close_1.png")));
+	
+	private int mouseX, mouseY;
 	
 	public BeatMuse() {
 		setUndecorated(true); // default top menu bar is invisible
@@ -29,8 +37,28 @@ public class BeatMuse extends JFrame {
 		setLayout(null);
 		
 		menuBar.setBounds(0, 0, 1920, 40);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed (MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged (MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
 		add(menuBar);
 		
+		closeButton.setBounds(50,50,30,30);
+		closeButton.setBorderPainted(false);
+		closeButton.setContentAreaFilled(false);
+		closeButton.setFocusPainted(false);
+		add(closeButton);
 		
 		Music introMusic = new Music("3rd Prototype - Dancefloor [NCS Release].mp3",true);
 		introMusic.start();
