766df8647ac7cf106c8779f94ce36fecef537827
Daniel <heyboy6059@gmail.com>
2019-08-29 16:06:27 +1000

    added left and right buttons on the first song introduction page

--- src/beatmuse/BeatMuse.java
+++ src/beatmuse/BeatMuse.java
@@ -27,12 +27,18 @@ public class BeatMuse extends JFrame {
 	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
 	
 
-	
+	private ImageIcon leftButtonBasicImage = new ImageIcon(Main.class.getResource("../images/leftButtonBasic.png"));
+	private ImageIcon leftButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/leftButtonEntered.png"));
+	private ImageIcon rightButtonBasicImage = new ImageIcon(Main.class.getResource("../images/rightButtonBasic.png"));
+	private ImageIcon rightButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/rightButtonEntered.png"));
 
 	
 	private JButton closeButton = new JButton(closeDefaultImage);
 	private JButton startButton = new JButton(startButtonBasicImage);
 	private JButton quitButton = new JButton(quitButtonBasicImage);
+	
+	private JButton leftButton = new JButton(leftButtonBasicImage);
+	private JButton rightButton = new JButton(rightButtonBasicImage);
 	
 	private Image background = new ImageIcon(Main.class.getResource("../images/MainBackground_title.jpg")).getImage();
 	private Image selectedImage = new ImageIcon(Main.class.getResource("../images/first_song_start_image.jpg")).getImage();
@@ -40,6 +46,8 @@ public class BeatMuse extends JFrame {
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/topbar.png")));
 	
 	private int mouseX, mouseY;
+	
+	private boolean isMainScreen = false;
 	
 	public BeatMuse() {
 		setUndecorated(true); // default top menu bar is invisible
@@ -110,6 +118,53 @@ public class BeatMuse extends JFrame {
 		
 		
 		
+		leftButton.setBounds(200,400,100,100);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEnteredImage);
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasicImage);
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//left button event
+			}
+		});
+		add(leftButton);
+		
+		rightButton.setBounds(1920-300,400,100,100);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(rightButtonEnteredImage);
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(rightButtonBasicImage);
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//right button event
+			}
+		});
+		add(rightButton);
+		
+		
+		
+		
 		startButton.setBounds(150,700,400,100);
 		startButton.setBorderPainted(false);
 		startButton.setContentAreaFilled(false);
@@ -130,6 +185,7 @@ public class BeatMuse extends JFrame {
 				startButton.setVisible(false);
 				quitButton.setVisible(false);
 				background = new ImageIcon(Main.class.getResource("../images/playBackground.jpg")).getImage();
+				isMainScreen = true;
 			}
 		});
 		add(startButton);
@@ -167,6 +223,9 @@ public class BeatMuse extends JFrame {
 	
 	public void screenDraw(Graphics g) {
 		g.drawImage(background, 0, 0, null); // good for adding images for general use?!
+		if(isMainScreen) {
+			g.drawImage(selectedImage, 550, 100, null);
+		}
 		paintComponents(g); // good for adding components that stay forever
 		this.repaint();
 	}
